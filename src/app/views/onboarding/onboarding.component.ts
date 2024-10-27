ef901ef5232e0fd15061f1c1c0aee94703913b80
Passitaz <zpasillas.ma@gmail.com>
2024-10-27 05:58:45 -0600

    Vista Onboarding

--- src/app/views/onboarding/onboarding.component.ts
+++ src/app/views/onboarding/onboarding.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit  } from '@angular/core';
+import 'bulma-carousel';
+
+declare var bulmaCarousel: any;
 
 @Component({
   selector: 'app-onboarding',
   templateUrl: './onboarding.component.html',
   styleUrls: ['./onboarding.component.css']
 })
-export class OnboardingComponent implements OnInit {
+export class OnboardingComponent implements OnInit, AfterViewInit {
+  showContinue: boolean = false; 
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    // Inicializar el carrusel después de que se haya renderizado la vista
+    const carousel = bulmaCarousel.attach('#carousel-demo', {
+      slidesToScroll: 1,
+      slidesToShow: 1
+    });
+
+    carousel.forEach((carouselItem: any) => {
+      carouselItem.on('index.change', (index: number) => {
+        // Mostrar el componente "Continuar" cuando el índice sea 2 (tercera imagen)
+        this.showContinue = (index === carouselItem.items.length - 1);
+      });
+    });
   }
 
 }
